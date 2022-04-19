8463500c52e8fb0d5fa4b71ade75fb3f5d94955b
Karim Leon <leon.karim08@gmail.com>
2022-04-18 23:24:42 -0400

    added functionality to btns

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const counterDisplay = document.querySelector("#count")
+const buttons = document.querySelectorAll('button')
+let countVal = 0; 
+counterDisplay.innerHTML = countVal
+
+Object.values(buttons).forEach(btn => {
+   btn.onclick = () => { 
+         if (btn.className == "maq i") counterDisplay.innerHTML = countVal++;
+         if (btn.className == "maq r") {
+                countVal = 0; 
+                counterDisplay.innerHTML = countVal;
+         }
+         if (btn.className == "maq d") counterDisplay.innerHTML = countVal--;
+   }
+})
+
+
+
+
+
+
+
+
+                                                                                                                       
